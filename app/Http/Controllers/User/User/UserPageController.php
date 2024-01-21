113e2cf65ae81c5dfd6cd7fc1149b7a2c0ea8c3c
Bocar MANGASSOUBA <bocardev@outlook.com>
2024-01-21 15:39:11 +0000

    Controller commit II

---
+++ app/Http/Controllers/User/User/UserPageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\User\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserPageController extends Controller
+{
+    //
+}
